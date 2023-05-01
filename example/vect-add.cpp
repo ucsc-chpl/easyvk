f6093247a40c28927a12d52bc9f500ab1c7bef39
Sean Siddens <seansiddens@gmail.com>
2023-05-01 11:08:39 -0700

    Loading spv at compile time

--- example/vect-add.cpp
+++ example/vect-add.cpp
@@ -1,13 +1,15 @@
 #include <vector>
 #include <iostream>
 #include <easyvk.h>
+#include <cassert>
+#include <vector>
 
 const int size = 4;
 
 int main(int argc, char* argv[]) {
 	auto instance = easyvk::Instance(true);
 	auto device = instance.devices().at(0);
-	std::cout << "Using device: " << device.properties().deviceName << "\n";
+	// std::cout << "Using device: " << device.properties().deviceName << "\n";
 
 	auto a = easyvk::Buffer(device, size);
 	auto b = easyvk::Buffer(device, size);
@@ -19,9 +21,11 @@ int main(int argc, char* argv[]) {
 		c.store(i, 0);
 	}
 	std::vector<easyvk::Buffer> bufs = {a, b, c};
-	const char* testFile = "vect-add.spv";
-
-	auto program = easyvk::Program(device, testFile, bufs);
+	const char* testFile = "build/vect-add.spv";
+	std::vector<uint32_t> spvCode =
+    #include "vect-add.cinit"
+    ;	
+	auto program = easyvk::Program(device, spvCode, bufs);
 	program.setWorkgroups(size);
 	program.setWorkgroupSize(1);
 	program.prepare();

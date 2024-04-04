df87eeb46c3cabfad5f7c1aeba318d4b1215e5fe
Reese Levine <reeselevine1@gmail.com>
2024-04-04 13:54:56 -0700

    try to get android build working

--- Android.mk
+++ Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE    := easyvk
-LOCAL_C_INCLUDES := src/ 
+LOCAL_C_INCLUDES := src/ volk/
 LOCAL_SRC_FILES := src/easyvk.cpp
 LOCAL_LDLIBS    += -lvulkan -llog
 
